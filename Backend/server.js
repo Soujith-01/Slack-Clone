da6f15a065b7c62da975495e53b63434da7d51fe
Soujith <24eg107c52@anurag.edu.in>
2026-05-04 11:33:20 +0530

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,51 @@
-console.log(hello)
+import exp from 'express'
+import {connect} from 'mongoose'
+import { config } from 'dotenv'
+import { createServer } from 'node:http'
+import { Server } from "socket.io";
+
+
+const app=exp()
+const server=createServer(app)
+const io = new Server(server);
+config()
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const connectDB = async()=>{
+    try{
+        await connect(process.env.DB_URL)
+        console.log('DB connected ')
+        const port=process.env.PORT || 4000;
+        server.listen(port,()=>console.log(`server listening in ${port}`))
+    }catch(error){
+        console.log("error in connecting",error.message)
+    }
+}
+
+connectDB()
+
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    //ValidationError
+    if(err.name=="ValidationError"){
+        return res.status(400).json({message:"Error occured",error:err})
+    }
+
+    //CastError
+    if(err.name=="CastError"){
+        return res.status(400).json({message:"Error occured",error:err})
+    }
+
+    //server side error
+    res.status(500).json({message:"error occured",error:err.message})
+})
+
+

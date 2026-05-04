cc568d9546741a5b980befa7e8ef9b78dffc25c9
mvishnuvardhanreddy06-oss <mvishnuvardhanreddy06@gmail.com>
2026-05-04 14:09:06 +0530

    updated

---
+++ Backend/models/ChannelModel.js
@@ -0,0 +1,24 @@
+import {Schema,model,Types} from "mongoose"
+
+const channelSchema=new Schema({
+    channelName:{
+        type:String,
+        requried:[true,"channel name is required"],
+        unique:[true,"channel already Exists"],
+    },
+    members:[{
+        type:Types.ObjectId,
+        ref:"user"
+    
+    }],
+    isChannelActive:{
+        type:String,
+        default:true,
+    },
+},{
+    timestamps:true,
+    versionKey:false,
+    strict:"throw"
+});
+//create model
+export const channelModel=model("channel",channelSchema)

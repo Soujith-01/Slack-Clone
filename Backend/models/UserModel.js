fcb4b3498fe1a1d318a5ea6e8d0d42b87644317c
Soujith <24eg107c52@anurag.edu.in>
2026-05-04 14:06:39 +0530

    updated

---
+++ Backend/models/UserModel.js
@@ -0,0 +1,40 @@
+import { Schema,model } from "mongoose";
+
+//create user schema(username,email,password,gender)
+const UserSchema = new Schema({
+    username:{
+        type:String,
+        required:[true,"Username required"],
+        minLength:[4,"minimum 4 characters"],
+        maxLength:[10,"maximum 10 characters"],
+        unique:[true,"username already exists"]
+    },
+    email:{
+        type:String,
+        required:[true,"email required"],
+        unique:[true,"email already exists"]
+    },
+    password:{
+        type:String,
+        required:[true,"password required"]
+    },
+    gender:{
+        type:String,
+        enum:['MALE','FEMALE','OTHERS'],
+        required:[true,"gender required"]
+    },
+    profileImage:{
+        type:String
+    },
+    isUserActive:{ //for soft deleting 
+        type:Boolean,
+        default:true
+    }
+},{
+    timestamps:true,
+    versionKey:false,
+    strict:'throw'
+}
+)
+
+export const UserModel=model('user',UserSchema)
